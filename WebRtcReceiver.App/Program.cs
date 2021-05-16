9ade91fdfe0d92d1bf096bc8d30ff3f4aa26c4ec
olivermertens <mertens@realtime-department.de>
2021-05-16 07:59:08 +0200

    Add project that receives WebRTC video and saves h264 data.

---
+++ WebRtcReceiver.App/Program.cs
@@ -0,0 +1,125 @@
+﻿using SIPSorcery.Net;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using VirtualStudio.Shared;
+using VirtualStudio.Shared.Connection;
+using VirtualStudio.Shared.DTOs;
+using WebRtcReceiver.Core;
+using VirtualStudio.Shared.Abstractions;
+using VirtualStudio.Shared.DTOs.WebRtc;
+using System.IO;
+
+namespace WebRtcReceiver.App
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            using (FileStream fileStream = File.Create("videodata.264"))
+            {
+                var virtualStudioClient = new VirtualStudioClient(fileStream);
+                await virtualStudioClient.ConnectAsync();
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class VirtualStudioClient : IWebRtcClient, IReceivingPeerListener
+    {
+        private SignalRVirtualStudioConnection _connection;
+        private readonly ReceivingPeer _peer;
+        private readonly Stream _outputStream;
+
+        public VirtualStudioClient(Stream outputStream)
+        {
+            _outputStream = outputStream ?? throw new ArgumentNullException(nameof(outputStream));
+            _peer = new ReceivingPeer(this, true);
+        }
+
+        public async Task ConnectAsync()
+        {
+            _connection = await SignalRVirtualStudioConnection.CreateAsync("localhost", 5001, true);
+            _connection.AddListener(this);
+
+            await _connection.JoinVirtualStudio("vs", new StudioComponentDto
+            {
+                Name = "Receiver",
+                IsPlaceholder = false,
+                Inputs = new List<StudioComponentEndpointDto>
+                {
+                    new StudioComponentEndpointDto(){IOType = EndpointIOType.Input, ConnectionType = "WebRTC", DataKind = DataKind.Video, Id = 1, Name = "VideoInput"}
+                }
+            });
+        }
+
+        #region IReceivingPeerListener
+        public void OnConnectionStateChanged(ReceivingPeer peer, RTCPeerConnectionState connectionState)
+        {
+            switch (connectionState)
+            {
+                case RTCPeerConnectionState.closed:
+                    // TODO
+                    break;
+            }
+        }
+
+        public void OnFrameReceived(ReceivingPeer peer, byte[] frame, uint timestamp)
+        {
+            _outputStream.Write(frame);
+        }
+
+        public void OnIceCandidate(ReceivingPeer peer, RTCIceCandidate iceCandidate)
+        {
+            _connection.SendIceCandidate(new IceCandidateArgs
+            {
+                ConnectionId = peer.ConnectionId,
+                CandidateDto = new RtcIceCandidateDto
+                {
+                    Candidate = iceCandidate.candidate,
+                    SdpMid = iceCandidate.sdpMid,
+                    SdpMLineIndex = iceCandidate.sdpMLineIndex,
+                    UsernameFragment = iceCandidate.usernameFragment
+                }
+            });
+        }
+        #endregion
+
+        #region IWebRtcClient
+        public Task RequestSdpOffer(SdpOfferRequestArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RequestSdpAnswer(SdpAnswerRequestArgs args)
+        {
+            var answer = _peer.GetSdpAnswer(args);
+            await _connection.SendSdpAnswer(new SdpAnswerResponseArgs
+            {
+                ConnectionId = _peer.ConnectionId,
+                SdpAnswer = answer.sdp,
+                UseInsertableStreams = false
+            });
+        }
+
+        public Task AddIceCandidate(IceCandidateArgs args)
+        {
+            _peer.AddIceCandidate(args);
+            return Task.CompletedTask;
+        }
+
+        public Task Connect(ConnectWebRtcCommandArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disconnect(DisconnectWebRtcCommandArgs args)
+        {
+            _peer.Disconnect(args);
+            return Task.CompletedTask;
+        }
+        #endregion
+    }
+}
---
+++ WebRtcReceiver.Core/ReceivingPeer.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using SIPSorcery.Net;
+using SIPSorceryMedia.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VirtualStudio.Shared.Abstractions;
+using VirtualStudio.Shared.DTOs.WebRtc;
+
+namespace WebRtcReceiver.Core
+{
+    public interface IReceivingPeerListener
+    {
+        void OnFrameReceived(ReceivingPeer peer, byte[] frame, uint timestamp);
+        void OnConnectionStateChanged(ReceivingPeer peer, RTCPeerConnectionState connectionState);
+        void OnIceCandidate(ReceivingPeer peer, RTCIceCandidate iceCandidate);
+    }
+
+    public class ReceivingPeer
+    {
+        private const int H264_SUGGESTED_FORMAT_ID = 100;
+
+        public int ConnectionId { get; private set; }
+
+        private RTCPeerConnection _peerConnection;
+
+        private readonly bool _noAudio;
+        private readonly IReceivingPeerListener _listener;
+
+        public ReceivingPeer(IReceivingPeerListener listener, bool noAudio)
+        {
+            _listener = listener ?? throw new ArgumentNullException(nameof(listener));
+            _noAudio = noAudio;
+        }
+
+        public RTCSessionDescriptionInit GetSdpAnswer(SdpAnswerRequestArgs args)
+        {
+            ConnectionId = args.ConnectionId;
+
+            RTCConfiguration config = new RTCConfiguration
+            {
+                X_UseRtpFeedbackProfile = true
+            };
+            var pc = new RTCPeerConnection(config);
+
+            // Add local receive only tracks. This ensures that the SDP answer includes only the codecs we support.
+            if (!_noAudio)
+            {
+                MediaStreamTrack audioTrack = new MediaStreamTrack(SDPMediaTypesEnum.audio, false,
+                    new List<SDPAudioVideoMediaFormat> { new SDPAudioVideoMediaFormat(SDPWellKnownMediaFormatsEnum.PCMU) }, MediaStreamStatusEnum.RecvOnly);
+                pc.addTrack(audioTrack);
+            }
+            // MediaStreamTrack videoTrack = new MediaStreamTrack(new VideoFormat(96, "VP8", 90000, "x-google-max-bitrate=5000000"), MediaStreamStatusEnum.RecvOnly);
+
+            // pc.OnVideoFrameReceived += _videoSink.GotVideoFrame;
+            // pc.OnVideoFormatsNegotiated += (formats) => _videoSink.SetVideoSinkFormat(formats.First());
+
+            pc.OnVideoFrameReceived += (endpoint, timestamp, frame, format) =>
+            {
+                Console.WriteLine($"On frame received: byte[{frame.Length}], ts {timestamp}");
+                _listener.OnFrameReceived(this, frame, timestamp);
+            };
+
+            pc.onicecandidate += (iceCandidate) =>
+            {
+                Console.WriteLine("On ice candidate");
+                _listener.OnIceCandidate(this, iceCandidate);
+            };
+
+            pc.onconnectionstatechange += (state) =>
+            {
+                Console.WriteLine($"Peer connection state change to {state}.");
+
+                if (state == RTCPeerConnectionState.failed)
+                {
+                    pc.Close("ice disconnection");
+                }
+                _listener.OnConnectionStateChanged(this, state);
+            };
+
+            pc.OnSendReport += (media, sr) => Console.WriteLine($"RTCP Send for {media}\n{sr.GetDebugSummary()}");
+            pc.oniceconnectionstatechange += (state) => Console.WriteLine($"ICE connection state change to {state}.");
+
+            var sdpOffer = SDP.ParseSDPDescription(args.SdpOffer);
+            //sdpOffer.Media.FirstOrDefault()?.
+            var videoMedia = sdpOffer.Media.FirstOrDefault(m => m.Media == SDPMediaTypesEnum.video);
+            var h264VideoFormat = videoMedia.MediaFormats.Values.First(m => m.Rtpmap == "H264/90000").ToVideoFormat();
+            var videoTrack = new MediaStreamTrack(h264VideoFormat, MediaStreamStatusEnum.RecvOnly);
+            pc.addTrack(videoTrack);
+
+            //var track = new MediaStreamTrack(SDPMediaTypesEnum.video, false, new List<SDPAudioVideoMediaFormat> { new SDPAudioVideoMediaFormat(SDPMediaTypesEnum.video, 102, "H264", 90000) });
+            //pc.addTrack(track);
+
+            var setRemoteDescriptionResult = pc.setRemoteDescription(new RTCSessionDescriptionInit
+            {
+                type = RTCSdpType.offer,
+                sdp = args.SdpOffer
+            });
+            //MediaStreamTrack videoTrack = new MediaStreamTrack(new VideoFormat(VideoCodecsEnum.H264, 102), MediaStreamStatusEnum.RecvOnly);
+            //pc.addTrack(videoTrack);
+
+            var answer = pc.createAnswer();
+            var setLocalDescriptionResult = pc.setLocalDescription(answer);
+
+            Console.WriteLine("SDP Offer:\n" + args.SdpOffer);
+            Console.WriteLine("SDP Answer 2:\n" + answer.sdp);
+
+            _peerConnection = pc;          
+            return answer;
+        }
+
+        public void AddIceCandidate(IceCandidateArgs args)
+        {
+            Console.WriteLine("Add ice candidate");
+            if (args.ConnectionId != ConnectionId)
+                throw new Exception();
+
+            _peerConnection.addIceCandidate(new RTCIceCandidateInit
+            {
+                candidate = args.CandidateDto.Candidate,
+                sdpMid = args.CandidateDto.SdpMid,
+                sdpMLineIndex = (ushort)args.CandidateDto.SdpMLineIndex,
+                usernameFragment = args.CandidateDto.UsernameFragment
+            });
+        }
+
+        public void Disconnect(DisconnectWebRtcCommandArgs args)
+        {
+            if (args.ConnectionId != ConnectionId)
+                throw new Exception();
+
+            _peerConnection.Dispose();
+        }
+    }
+}
